fc85c146711956572028be55a024bd1e4c16fb9d
13072064 <dkennedy15@qub.ac.uk>
2019-03-05 19:20:04 +0000

    roll dice method in turnEngine

--- Technopoly_v1/Technopoly/src/g3/technopoly/TurnEngine.java
+++ Technopoly_v1/Technopoly/src/g3/technopoly/TurnEngine.java
@@ -1,15 +1,18 @@
 package g3.technopoly;
 
 public class TurnEngine {
-	
-	//instance vars
-	private boolean gameInPlay =true;
-	protected int playerTurn = 0; 
+
+	// instance vars
+	private boolean gameInPlay = true;
+	protected int currentPlayer = 0;
+	private int doublesCount = 0;
 	private int lastPlayer, lastSpace;
-	
-	//getters and setters
+	private static final double TAX = 1000;
+
+	// getters and setters
 	/**
 	 * Get the status of the game, in play or not
+	 * 
 	 * @return the gameInPlay (boolean)
 	 */
 	public boolean isGameInPlay() {
@@ -25,21 +28,23 @@ public class TurnEngine {
 
 	/**
 	 * Get the player who's turn it is
+	 * 
 	 * @return the playerTurn
 	 */
 	public int getPlayerTurn() {
-		return playerTurn;
+		return currentPlayer;
 	}
 
 	/**
 	 * @param Set the next players turn
 	 */
 	public void setPlayerTurn(int playerTurn) {
-		this.playerTurn = playerTurn;
+		this.currentPlayer = playerTurn;
 	}
 
 	/**
 	 * Get the number of the last player who had a turn
+	 * 
 	 * @return the lastPlayer
 	 */
 	public int getLastPlayer() {
@@ -48,6 +53,7 @@ public class TurnEngine {
 
 	/**
 	 * Set the player who just had their go
+	 * 
 	 * @param lastPlayer the lastPlayer to set
 	 */
 	public void setLastPlayer(int lastPlayer) {
@@ -56,6 +62,7 @@ public class TurnEngine {
 
 	/**
 	 * Get the last space that a player was on
+	 * 
 	 * @return the lastSpace
 	 */
 	public int getLastSpace() {
@@ -64,15 +71,42 @@ public class TurnEngine {
 
 	/**
 	 * Set the last space that a player was on
+	 * 
 	 * @param lastSpace the lastSpace to set
 	 */
 	public void setLastSpace(int lastSpace) {
 		this.lastSpace = lastSpace;
 	}
-	
-	public void rollDice() {
+	//methods
+	/**
+	 * Method to roll the dice and move the player
+	 * Validation: check if the doubles have been rolled
+	 * three times in a row and fine the player if so.
+	 * @throws Exception
+	 */
+	public void rollDice() throws Exception {
+		
+		int dice1 = Dice.throwDice();
+		int dice2 = Dice.throwDice();
+		int moveAmount = dice1 + dice2;
+		
+		if(dice1 == dice2) {
+			System.out.println("You rolled doubles! You get another turn when this one is over!");
+			doublesCount++;
+		}else {
+			System.out.println("You rolled a " + moveAmount);
+			doublesCount = 0;
+		}
+		if(doublesCount == 3) {
+			System.out.println("\nUh oh, three doubles in a row, the taxman wants his dues. You've been fined £"+TAX+"\n");
+			Bank.subtract(currentPlayer, TAX);
+			doublesCount = 0;
+			System.out.println("\nYour new balance is £" + gameAdmin.players.get(playerNumber).getBalanceAmount() + "\n");
+		}
 		
 	}
+	
+
 	
 	
 
