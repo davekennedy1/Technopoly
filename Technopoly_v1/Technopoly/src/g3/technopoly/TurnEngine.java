cdafb870e092aed2ef5186f81b24b77df489a8c5
13072064 <dkennedy15@qub.ac.uk>
2019-03-07 08:30:49 +0000

    Working move and doubles check on TurnEngine

--- Technopoly_v1/Technopoly/src/g3/technopoly/TurnEngine.java
+++ Technopoly_v1/Technopoly/src/g3/technopoly/TurnEngine.java
@@ -2,116 +2,161 @@ package g3.technopoly;
 
 public class TurnEngine {
 
-	// instance vars
-	private boolean gameInPlay = true;
-	protected int currentPlayer = 0;
-	private int doublesCount = 0;
-	private int lastPlayer, lastSpace;
+	// Instance vars
+	protected int currentPlayer;
+	private int boardSpaces;
+	private int currentPlayerSpace;
 	private static final double TAX = 1000;
 
-	// getters and setters
+	// Constructors
 	/**
-	 * Get the status of the game, in play or not
-	 * 
-	 * @return the gameInPlay (boolean)
+	 * Empty Constructor
 	 */
-	public boolean isGameInPlay() {
-		return gameInPlay;
+	public TurnEngine() {
 	}
 
 	/**
-	 * @param Set the status of the game in play (boolean)
+	 * Constructor with args for generating a turn
+	 * 
+	 * @param currentPlayer      (int)
+	 * @param boardSpaces        (int)
+	 * @param currentPlayerSpace (int)
 	 */
-	public void setGameInPlay(boolean gameInPlay) {
-		this.gameInPlay = gameInPlay;
+	public TurnEngine(int currentPlayer, int boardSpaces, int currentPlayerSpace) {
+		this.currentPlayer = currentPlayer;
+		this.boardSpaces = boardSpaces;
+		this.currentPlayerSpace = currentPlayerSpace;
 	}
 
+	
+	// Getters and setters
 	/**
-	 * Get the player who's turn it is
-	 * 
-	 * @return the playerTurn
+	 * Get the number of the current player
+	 * @return currentPlayer (int)
 	 */
-	public int getPlayerTurn() {
+	public int getCurrentPlayer() {
 		return currentPlayer;
 	}
 
 	/**
-	 * @param Set the next players turn
+	 * Set the current player number
+	 * @param currentPlayer (int)
 	 */
-	public void setPlayerTurn(int playerTurn) {
-		this.currentPlayer = playerTurn;
+	public void setCurrentPlayer(int currentPlayer) {
+		this.currentPlayer = currentPlayer;
 	}
 
 	/**
-	 * Get the number of the last player who had a turn
-	 * 
-	 * @return the lastPlayer
+	 * Get the number of spaces on the board
+	 * @return boardSpaces (int)
 	 */
-	public int getLastPlayer() {
-		return lastPlayer;
+	public int getBoardSpaces() {
+		return boardSpaces;
 	}
 
 	/**
-	 * Set the player who just had their go
-	 * 
-	 * @param lastPlayer the lastPlayer to set
+	 * Set the number of spaces on the board
+	 * @param boardSpaces (int)
 	 */
-	public void setLastPlayer(int lastPlayer) {
-		this.lastPlayer = lastPlayer;
+	public void setBoardSpaces(int boardSpaces) {
+		this.boardSpaces = boardSpaces;
 	}
 
 	/**
-	 * Get the last space that a player was on
-	 * 
-	 * @return the lastSpace
+	 * Get the position of the current player on the board
+	 * @return currentPlayerSpace (int)
 	 */
-	public int getLastSpace() {
-		return lastSpace;
+	public int getCurrentPlayerSpace() {
+		return currentPlayerSpace;
 	}
 
 	/**
-	 * Set the last space that a player was on
-	 * 
-	 * @param lastSpace the lastSpace to set
+	 * Set the players position on the board
+	 * @param currentPlayerSpace (int)
 	 */
-	public void setLastSpace(int lastSpace) {
-		this.lastSpace = lastSpace;
+	public void setCurrentPlayerSpace(int currentPlayerSpace) {
+		this.currentPlayerSpace = currentPlayerSpace;
 	}
-	//methods
+
+
+	// Methods
 	/**
-	 * Method to roll the dice and move the player
-	 * Validation: check if the doubles have been rolled
-	 * three times in a row and fine the player if so.
+	 * Method to roll the dice and move the player 
+	 * Validation: check if doubles have been rolled.
+	 * Rolling doubles gives the current player another go.
+	 * However rolling doubles three times in a row results 
+	 * in a fine for the player.
+	 * 
 	 * @throws Exception
 	 */
 	public void rollDice() throws Exception {
-		
+
 		int dice1 = Dice.throwDice();
 		int dice2 = Dice.throwDice();
 		int moveAmount = dice1 + dice2;
+
+		System.out.println("you were on space " + this.currentPlayerSpace);
 		
-		if(dice1 == dice2) {
-			System.out.println("You rolled doubles! You get another turn when this one is over!");
-			doublesCount++;
+		System.out.println("You rolled a " + dice1 + " and a " + dice2 + " giving you " + moveAmount);
+		
+		if (dice1 == dice2 && Driver.game.getDoublesCounter()!= 2) {
+			System.out.println("Because you rolled doubles, you get another turn after this one!");
+			
+			Driver.game.setDoublesCounter(true);
+			
+			movePlayer(moveAmount);
+		}else if(dice1 == dice2 && Driver.game.getDoublesCounter()== 2) {
+			System.out.println("Three doubles in a row! Well the good news is wealthy capitalists don't go to jail, they just get a slap on the wrist!");
+			System.out.println("You've been fined £" + TAX);
+			if(Bank.checkFunds(this.currentPlayer, TAX)) {
+				Bank.subtract(this.currentPlayer, TAX);
+				Driver.game.setDoublesCounter(false);
+				movePlayer(moveAmount);
+			}else {
+				//NEEDS A METHOD TO TERMINATE THE GAME!!!!!!!!!!!!!!!!!!!!!!!!
+				System.out.println("YOU IS BROKE!!! GAME OVER");
+				Driver.game.setGameInPlay(false);
+			}
+			
 		}else {
-			System.out.println("You rolled a " + moveAmount);
-			doublesCount = 0;
-		}
-		if(doublesCount == 3) {
-			System.out.println("\nUh oh, three doubles in a row, the taxman wants his dues. You've been fined £"+TAX+"\n");
-			Bank.subtract(currentPlayer, TAX);
-			doublesCount = 0;
-			System.out.println("\nYour new balance is £" + gameAdmin.players.get(playerNumber).getBalanceAmount() + "\n");
+			Driver.game.setDoublesCounter(false);
+			movePlayer(moveAmount);
 		}
 		
+		
+
 	}
-	
 
-	
-	
+	/**
+	 * Method to move the player the amount of spaces required by passing an amount.
+	 * Validation: Check if the player has passed the InvestNI space and 
+	 * work out the new space that the player has landed on.
+	 * 
+	 * @param moveAmount (int)
+	 */
+	public void movePlayer(int moveAmount) {
+		int lapBoardBy;
+		
+		if((this.currentPlayerSpace + moveAmount)<boardSpaces) {
+			this.currentPlayerSpace += moveAmount;
+			Driver.players.get(this.currentPlayer).setPositionInBoard(this.getCurrentPlayerSpace());
+			System.out.println("You landed on Space: " + this.currentPlayerSpace +"\n");
+		}else{
+			lapBoardBy = (this.currentPlayerSpace + moveAmount) - (boardSpaces);
+			this.currentPlayerSpace = lapBoardBy;
+			
+			Driver.players.get(this.currentPlayer).setPositionInBoard(this.getCurrentPlayerSpace());
+			System.out.println("You landed on Space: " + this.currentPlayerSpace);
+			if(this.currentPlayerSpace == 0) {
+				System.out.println("You landed on InvestNI, you get £" + InvestNI.getInvestmentAmount());
+				Driver.investNI.addInvestment(this.currentPlayer);
+				System.out.printf("New Balance: £%,.0f\\n\n", Driver.players.get(currentPlayer).getBalanceAmount());
+			}else {
+				System.out.printf("You passed InvestNI, you get £%,.0f\n", InvestNI.getInvestmentAmount());
+				Driver.investNI.addInvestment(this.currentPlayer);
+				System.out.printf("New Balance: £%,.0f\n\n", Driver.players.get(currentPlayer).getBalanceAmount());
+			}
+		}
+	}
 
-
-
-	
-	
 }
