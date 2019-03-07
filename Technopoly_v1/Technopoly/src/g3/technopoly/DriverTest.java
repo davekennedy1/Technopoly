c244858e64d16919ebbd5bd4f357a18226866a26
13072064 <dkennedy15@qub.ac.uk>
2019-03-07 19:07:05 +0000

    test7

--- Technopoly_v1/Technopoly/src/g3/technopoly/DriverTest.java
+++ Technopoly_v1/Technopoly/src/g3/technopoly/DriverTest.java
@@ -14,6 +14,8 @@ public class DriverTest {
 		int staffNumber = ((StartupSpace)GameAdmin.board.getSpaces().get(4)).getStaff();
 		kjhkjhlkjdsaflkjaIsmaeladsl;fjkalkadsf
 		Can you do me a favour can you push something now?
+				
+				man in the mirror
 
 	}
 
