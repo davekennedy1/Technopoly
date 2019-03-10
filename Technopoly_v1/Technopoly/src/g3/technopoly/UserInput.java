6dcf4242881c24becccbaa54713386ee512714e0
Timothy Lewis <tleiws04@qub.ac.uk>
2019-03-10 14:25:48 +0000

    Fixed another small error

--- Technopoly_v1/Technopoly/src/g3/technopoly/UserInput.java
+++ Technopoly_v1/Technopoly/src/g3/technopoly/UserInput.java
@@ -83,7 +83,7 @@ public class UserInput {
 			try {
 			System.out.println("Hint: Empty names are not allowed");
 				userName = sc1.nextLine();
-				sc1.nextLine(); 
+			
 
 			} catch (NullPointerException e) {
 				sc1.next();
