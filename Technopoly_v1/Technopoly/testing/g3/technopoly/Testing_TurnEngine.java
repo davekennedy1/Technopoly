3c1cfeec7deacd5dca6a95ba2ff4887cc382b274
Ismael Florit <ismaelflorit@gmail.com>
2019-03-11 18:13:46 +0000

    Draft Pass test (without setup/teardown)

--- Technopoly_v1/Technopoly/testing/g3/technopoly/Testing_TurnEngine.java
+++ Technopoly_v1/Technopoly/testing/g3/technopoly/Testing_TurnEngine.java
@@ -2,28 +2,27 @@ package g3.technopoly;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class Testing_TurnEngine {
 
+	
 	TurnEngine turnEngine = new TurnEngine();
 	int fieldCost = 1000;
+	
 
-	@Before
-	public void setUp() throws Exception {
-		GameAdmin.board.populateBoard();
-
-	}
 
 	@Test
 	public void test_HireStaffInIncrementsOfOne() {
+		GameAdmin.board.populateBoard();
 		GameAdmin.players.add(new Player("player1", 0, 150000));
-		((StartupSpace) GameAdmin.board.getSpaces().get(1)).setStaff(0);
-		
+
 		turnEngine.hiresStaff(1, 0, fieldCost);
-		int expected = 1;
-		int actual = ((StartupSpace) GameAdmin.board.getSpaces().get(1)).getStaff();
+		int expected = 1; 
+		int actual = ((StartupSpace)GameAdmin.board.getSpaces().get(1)).getStaff();
 		assertEquals(expected, actual);
 		
 		turnEngine.hiresStaff(1, 0, fieldCost);
@@ -43,13 +42,18 @@ public class Testing_TurnEngine {
 
 	}
 	
-//	@Test
-//	public void test_PlayerDeclareBankrupt() {
-//	GameAdmin.players.add(new Player("player2", 0, 0));
+	@Test
+	public void test_PlayerDeclareBankrupt() {
+		GameAdmin.players.add(new Player("player2", 0, 0));
+		turnEngine.setCurrentPlayer(1);
+		turnEngine.setCurrentPlayerSpace(1);
+		((StartupSpace) GameAdmin.board.getSpaces().get(1)).setPlayerOwner(0);
+		turnEngine.paysLicenceFee(500);
+		
+		boolean expected = false;
+		boolean actual = GameAdmin.game.isGameInPlay();
+		assertEquals(expected, actual);
 
-//		turnEngine.setCurrentPlayer(1);
-//		System.out.println(GameAdmin.players.get(turnEngine.getCurrentPlayer()).getBalanceAmount());
-//		turnEngine.paysLicenceFee(500);
-//	}
+	}
 
 }
