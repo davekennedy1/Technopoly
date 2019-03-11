4d8828f9272c5dd040c0474a7896a4a402343310
Ismael Florit <ismaelflorit@gmail.com>
2019-03-11 11:34:50 +0000

    Moved testing folder back into source. Added Testing_takesOverStartup()

---
+++ Technopoly_v1/Technopoly/testing/g3/technopoly/Testing_takesOverStartup.java
@@ -0,0 +1,19 @@
+package g3.technopoly;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Testing_takesOverStartup {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTakesOverStartup() {
+		fail("Not yet implemented");
+	}
+
+}
